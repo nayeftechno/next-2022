fae7ba5d445b906f4e642d398d9674e870219418
nayeftechno <nayeftechno@gmail.com>
2021-12-29 15:43:40 +0200

    Commit Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import Navbar from '../components/Navbar';
-import { MiddleWaresValues } from '../middleware';
 
-interface Props extends AppProps<MiddleWaresValues> {
-  pageProps: MiddleWaresValues;
-}
-
-function MyApp({ Component, pageProps }: Props  ) : JSX.Element {
+function MyApp({ Component, pageProps }: AppProps  ) : JSX.Element {
   return (<>
     <Navbar />
     <Layout>
