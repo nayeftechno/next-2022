acded9892980907a71493beda636bdfdd4a0fe1a
nayeftechno <nayeftechno@gmail.com>
2021-12-29 14:49:34 +0200

    Commit Changes

--- components/EmployeesList.tsx
+++ components/EmployeesList.tsx
@@ -1,30 +1,11 @@
-import { useEffect, useState } from 'react';
-import { EmployeeClient } from '../packages/web-sdk/src/clients/employee.client';
-import CompanyStore from '../packages/web-sdk/src/stores/employee-store';
-import EmployeeStore from '../packages/web-sdk/src/stores/employee-store/employee.store';
-const store = new CompanyStore();
+import { IEmployee } from '../interfaces/IEmployee';
+import { EmployeesStateProps as EmployeesListProps} from '../pages/employees/index';
 
-function EmployeesList(): JSX.Element {
-
-    const [state, setState] = useState({ loading: false, employees: [] as any, error: '' });
-    useEffect(() => {
-
-        alert(store.employeeStore.getLoading)
-        async function getEmployees() {
-            setState({ ...state, loading: true, error: '' });
-            EmployeeClient.getEmployees()?.then((response: any) => {
-                setState({ ...state, loading: false, employees: [...response], error: '' });
-            }).catch((error) => {
-                setState({ ...state, loading: false, employees: [], error: error.message });
-            });
-        };
-        getEmployees();
-    }, []);
-    const { loading, employees, error } = state;
+function EmployeesList({ loading, employees, error }: EmployeesListProps): JSX.Element {
     return (<>
         {loading && !error ? (<h4>Loading...</h4>) : (<ul className='list-group'>
             {
-                employees.map((employee: any) => {
+                employees.map((employee: IEmployee) => {
                     const { id, name } = employee;
                     return (<li key={id} className='list-group-item'>{name}</li>);
                 })
--- pages/employees/index.tsx
+++ pages/employees/index.tsx
@@ -1,16 +1,37 @@
-import type {NextPage} from 'next';
+import { useEffect, useState } from 'react';
+import type { NextPage } from 'next';
+import { EmployeeClient } from '../../packages/web-sdk/src/clients/employee.client';
 import RenderHead from '../../components/RenderHead';
 import EmployeesList from '../../components/EmployeesList';
+import { IEmployee } from '../../interfaces/IEmployee';
 
-const EmployeesPage : NextPage = () : JSX.Element=>{
-    return(<>
-    <RenderHead title='Employees'/>
-    <div className='row'>
-    <h4>Employees Page</h4>
-        <div className='col-md-12'>
-            <EmployeesList/>
+export type EmployeesStateProps = {
+    employees: IEmployee[];
+    loading: boolean;
+    error: string;
+};
+
+const EmployeesPage: NextPage = (): JSX.Element => {
+    const [state, setState] = useState<EmployeesStateProps>({ employees: [], loading: false, error: '' });
+    useEffect(() => {
+        function getEmployees() {
+            setState({ ...state, loading: true });
+            EmployeeClient.getEmployees()?.then((response: any) => {
+                setState({ ...state, loading: false, employees: response, error: '' });
+            }).catch((error) => {
+                setState({ ...state, loading: false, employees: [], error: error.message });
+            });
+        };
+        getEmployees();
+    }, []);
+    return (<>
+        <RenderHead title='Employees' />
+        <div className='row'>
+            <h4>Employees Page</h4>
+            <div className='col-md-12'>
+                <EmployeesList {...state} />
+            </div>
         </div>
-    </div>
     </>);
 };
 
