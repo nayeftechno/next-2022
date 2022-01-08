934e39f307ac3e54a0780d6d85010d1c664a4971
nayeftechno <nayeftechno@gmail.com>
2022-01-08 10:07:00 +0200

    Commit changes

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import {useRouter} from 'next/router';
 
 export default function PostDetailsPage({ post }: any): JSX.Element {
-    const {isFallback} = useRouter();
-    if(isFallback){
-        return(<h1>LOADING....</h1>);
-    }
+    //const {isFallback} = useRouter();
+    // if(isFallback){
+    //     return(<h1>LOADING....</h1>);
+    // }
     const {id,title,body} = post;
     return (<>
         <RenderHead title={`Post Details ${id}`} />
@@ -30,13 +30,18 @@ export async function getStaticPaths() {
     ];
     return {
         paths,
-        fallback : true
+        fallback : 'blocking'
     };
 };
 export async function getStaticProps(context: any) {
     const { params: { postId } } = context;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     const post = await response.json();
+    if(!post.id){
+        return{
+            notFound : true
+        };
+    }
     return {
         props: {
             post
