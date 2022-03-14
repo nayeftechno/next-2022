551cfb41542b676e4406326902066ad58045a8cf
nayeftechno <nayeftechno@gmail.com>
2022-03-14 13:14:08 +0300

    Commit changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,11 +10,11 @@ function Navbar(): JSX.Element {
                         <a className="nav-link">Home</a>
                     </Link>
                 </li>
-                <li className='nav-item'>
+                {/* <li className='nav-item'>
                     <Link href={'/about'}>
                         <a className="nav-link">About</a>
                     </Link>
-                </li>
+                </li> */}
                 {/* <li className='nav-item'>
                     <Link href={'/employees'}>
                         <a className="nav-link">Employees</a>
@@ -25,7 +25,7 @@ function Navbar(): JSX.Element {
                         <a className="nav-link">Extra Employees</a>
                     </Link>
                 </li> */}
-                <li className='nav-item'>
+                {/* <li className='nav-item'>
                     <Link href={'/users'}>
                         <a className="nav-link">Users</a>
                     </Link>
@@ -58,6 +58,16 @@ function Navbar(): JSX.Element {
                 <li className='nav-item'>
                     <Link href={'/pro'}>
                         <a className="nav-link">Pro</a>
+                    </Link>
+                </li> */}
+                <li className='nav-item'>
+                    <Link href={'/without-swr'}>
+                        <a className="nav-link">Without SWR</a>
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link href={'/with-swr'}>
+                        <a className="nav-link">With SWR</a>
                     </Link>
                 </li>
             </ul>
---
+++ pages/without-swr/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+const IndexPage: NextPage = (): JSX.Element => {
+  return(<div className="row">
+      <h4>Without SWR</h4>
+      <div className="col-md-12"></div>
+  </div>);
+};
+
+export default IndexPage;
